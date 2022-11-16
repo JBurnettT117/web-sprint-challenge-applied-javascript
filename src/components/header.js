ca573825ef896b6755a1e590da149d4cb577d54a
Josh Burnett <joshuaburnett429@gmail.com>
2022-11-16 16:56:17 -0600

    completed second task

--- src/components/header.js
+++ src/components/header.js
@@ -32,6 +32,8 @@ const Header = (title, date, temp) => {
 }
 
 const headerAppender = (selector) => {
+  
+  document.querySelector(selector).appendChild(Header("i", "H", "!"));
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
